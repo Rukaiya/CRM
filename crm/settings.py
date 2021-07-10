c756aad85670fd1a8a58b8c9477ee504ccda5604
Rukaiya <59635237+Rukaiya@users.noreply.github.com>
2021-07-10 10:59:26 +0600

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'CRM_TEST',
         'USER': 'postgres',
-        'PASSWORD': 'ra110908',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432'
     }
